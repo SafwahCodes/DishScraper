d122c83762584a5b014b469deeb5b168bc8ae77c
safwah <safwahcodes@gmail.com>
2022-10-14 12:19:42 +0100

    added base class implementation

--- scrape_me.py
+++ scrape_me.py
@@ -13,12 +13,16 @@ class ScrapeMe:
         return BeautifulSoup(response.content, features=parser)
     
     def scrape(self, soup: BeautifulSoup) -> dict[str, Union[str, list[str]]]:
-        raise NotImplementedError
+        result: dict[str, Union[str, list[str]]] = {}
+        result['title'] = self.scrape_title(soup)
+        result['ingredients'] = self.scrape_ingredients(soup)
+        result['instructions'] = self.scrape_instructions(soup)
+        return result
     
     def scrape_title(self, soup: BeautifulSoup) -> str:
         raise NotImplementedError
     
-    def scrape_ingredients(self, soup: BeautifulSoup, remove_price: bool=True, separator: str='(') -> list[str]:
+    def scrape_ingredients(self, soup: BeautifulSoup) -> list[str]:
         raise NotImplementedError
 
     def scrape_instructions(self, soup:BeautifulSoup) -> list[str]:
