982463b0b6a69f4abd080db0b8fb7dd82916412d
safwah <safwahcodes@gmail.com>
2022-10-14 12:36:00 +0100

    added option to scrap old style recipes from jo website

--- scrape_me_jo.py
+++ scrape_me_jo.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
+from typing import Union
 
 from scrape_me import ScrapeMe
     
 class ScrapeMeJO(ScrapeMe):
+    
+    def scrape(self, soup: BeautifulSoup, old_style: bool=False) -> dict[str, Union[str, list[str]]]:
+        result: dict[str, Union[str, list[str]]] = {}
+        result['title'] = self.scrape_title(soup)
+        result['ingredients'] = self.scrape_ingredients(soup)
+        result['instructions'] = self.scrape_instructions(soup, old_style)
+        return result
     
     def scrape_title(self, soup: BeautifulSoup) -> str:
         return soup.find("h1").get_text()
@@ -14,10 +22,13 @@ class ScrapeMeJO(ScrapeMe):
             result.append(' '.join(elem.get_text().split()))
         return result
 
-    def scrape_instructions(self, soup:BeautifulSoup) -> list[str]:
-        soup_instructions = soup.find(class_="recipeSteps")
+    def scrape_instructions(self, soup:BeautifulSoup, old_style: bool=False) -> list[str]:
         result: list[str] = []
-        #result.append(soup_instructions)
-        for id, elem in enumerate(soup_instructions.find_all("li")):
-            result.append(str(id+1) + ". " + elem.get_text())
+        if (old_style):
+            result.append(soup.find(class_="method-p").find("div").getText())
+        else:
+            soup_instructions = soup.find(class_="recipeSteps")
+            for id, elem in enumerate(soup_instructions.find_all("li")):
+                result.append(str(id+1) + ". " + elem.get_text())
+        
         return result
