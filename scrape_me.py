0010027f9d2f3d45961443ed0ced5cc93da39135
SafwahCodes <SafwahCodes@gmail.com>
2023-08-01 18:56:10 +0100

    fixed formating of comments

--- scrape_me.py
+++ scrape_me.py
@@ -11,22 +11,22 @@ class ScrapeMe:
 	def __init__(self):
 		pass
 
-    ## Returns a requests.Response object from a given URL.
-    # @param url The URL to get a response from.
-    # @return A requests.Response object returned by sending a GET request to url.
+  ## Returns a requests.Response object from a given URL.
+  # @param url The URL to get a response from.
+  # @return A requests.Response object returned by sending a GET request to url.
 	def fetch(self, url: str) -> requests.models.Response:
 		return requests.get(url)
 	
-    ## Converts a requests.Response object into a BeautifulSoup object.
-    # @param response A requests.Reponse object to convert.
-    # @param parser Specifies the type of parsing to be done. Default parses html.
-    # @return A BeautifulSoup object containing the contents of the requests.Response object.
+  ## Converts a requests.Response object into a BeautifulSoup object.
+  # @param response A requests.Reponse object to convert.
+  # @param parser Specifies the type of parsing to be done. Default parses html.
+  # @return A BeautifulSoup object containing the contents of the requests.Response object.
 	def convert_to_bs4(self, response: requests.models.Response, parser: str="html.parser") -> BeautifulSoup:
 		return BeautifulSoup(response.content, features=parser)
 	
-    ## Scrapes the recipe title, ingredients and instructions from a BeautifulSoup object.
-    # @param soup A BeautifulSoup object containing the recipe to scrape.
-    # @return A dictionary containing the title, ingredients and instructions of the recipe.
+  ## Scrapes the recipe title, ingredients and instructions from a BeautifulSoup object.
+  # @param soup A BeautifulSoup object containing the recipe to scrape.
+  # @return A dictionary containing the title, ingredients and instructions of the recipe.
 	def scrape(self, soup: BeautifulSoup) -> dict[str, Union[str, list[str]]]:
 		result: dict[str, Union[str, list[str]]] = {}
 		result['title'] = self.scrape_title(soup)
@@ -34,20 +34,21 @@ class ScrapeMe:
 		result['instructions'] = self.scrape_instructions(soup)
 		return result
 			
-    ## Scrapes the recipe title from a BeautifulSoup object
-    # @param soup A BeautifulSoup object to scrape from.
-    # @return A string representing the recipe title. Raises a NotImplementedError if not overriden in child classes.
+  ## Scrapes the recipe title from a BeautifulSoup object
+  # @param soup A BeautifulSoup object to scrape from.
+  # @return A string representing the recipe title. Raises a NotImplementedError if not overriden in child classes.
 	def scrape_title(self, soup: BeautifulSoup) -> str:
 		raise NotImplementedError
 			
-    ## Scrapes the recipe ingredients from a BeautifulSoup object
-    # @param soup A BeautifulSoup object to scrape from.
-    # @return A list of strings representing the recipe ingredients. Raises a NotImplementedError if not overriden in child classes.
+  ## Scrapes the recipe ingredients from a BeautifulSoup object
+  # @param soup A BeautifulSoup object to scrape from.
+  # @return A list of strings representing the recipe ingredients. Raises a NotImplementedError if not overriden in child classes.
 	def scrape_ingredients(self, soup: BeautifulSoup) -> list[str]:
 		raise NotImplementedError
 
-    ## Scrapes the recipe instructions from a BeautifulSoup object
-    # @param soup A BeautifulSoup object to scrape from.
-    # @return A list of strings representing the recipe instructions. Raises a NotImplementedError if not overriden in child classes.
+  ## Scrapes the recipe instructions from a BeautifulSoup object
+  # @param soup A BeautifulSoup object to scrape from.
+  # @return A list of strings representing the recipe instructions. Raises a NotImplementedError if not overriden in child classes.
 	def scrape_instructions(self, soup:BeautifulSoup) -> list[str]:
 		raise NotImplementedError
+	
